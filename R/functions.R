f7d41590368204e19e95cd39267e6c09e371b37a
NewGraphEnvironment <al@newgraphenvironment.com>
2021-07-24 15:45:27 -0700

    tweak name of pdf and add mapsheet to gpx

--- R/functions.R
+++ R/functions.R
@@ -53,7 +53,8 @@ make_html_tbl <- function(df) {
   df2 <- select(df, -blue_line_key, -linear_feature_id, -lat, -long,
                 -sub5, -name, -desc, -shape, -watershed_code_20k, watershed_code_50k,-color) %>%
     sf::st_drop_geometry() %>%
-    janitor::remove_empty() #, -shape, -color, -label
+    janitor::remove_empty()
+    #, -shape, -color, -label
   df %>%
     mutate(html_tbl = knitr::kable(df2, 'html', escape = F) %>%
              kableExtra::row_spec(0:nrow(df2), extra_css = "border: 1px solid black;") %>% # All cells get a border
